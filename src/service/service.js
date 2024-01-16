ab144bd8c7540000a72425ca2355c21b57479ec1
VMang0 <valeriakorolkova2703@gmail.com>
2024-01-16 17:11:13 +0300

    feat: marvel service

---
+++ src/service/service.js
@@ -0,0 +1,37 @@
+class Service {
+  _apiBaseUrl = 'https://gateway.marvel.com:443/v1/public/';
+  _key = '2531cfd5eff0fd2c7821aac82da2cb83';
+  _apiKey = `apikey=${this._key}`;
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status ${res.status}`)
+    }
+    return await res.json();
+  }
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBaseUrl}characters?limit=9&offset=300&${this._apiKey}`);
+    return res.data.results.map(this._transformChar)
+  }
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBaseUrl}characters/${id}?${this._apiKey}`)
+    return this._transformChar(res.data.results[0]);
+  }
+
+  _transformChar = (char) => {
+    const oldDescription = char.description;
+    const transformDescription = oldDescription.length > 287
+      ? oldDescription.slice(0, 227) + '...'
+      : 'Description not found'
+    return {
+      name: char.name,
+      description: transformDescription,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
+  }
+}
+
+export default Service;
