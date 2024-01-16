ba7f37adaf37e936c264d58c0c3f72ba2738d8ad
VMang0 <valeriakorolkova2703@gmail.com>
2024-01-16 21:00:20 +0300

    style: not image picture with contain objectfit

--- src/components/random-char-block/random-char/random-char.js
+++ src/components/random-char-block/random-char/random-char.js
@@ -4,9 +4,10 @@ import './random-char.scss';
 
 const RandomChar = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  const objectFit = thumbnail.split('/').includes('image_not_available.jpg') ? 'contain' : 'cover';
   return (
     <div className="randomChar">
-      <img src={thumbnail} alt={`${name} character`} />
+      <img src={thumbnail} alt={`${name} character`} style={{ objectFit }}/>
       <div className="randomChar__info">
         <p className="randomChar__info__charname">{name}</p>
         <p className="randomChar__info__details">
